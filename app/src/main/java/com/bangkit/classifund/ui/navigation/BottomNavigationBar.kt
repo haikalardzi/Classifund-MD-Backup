64581105499bc65c060a5b1e9cf9f705da43d989
haikalardzi <haikalardzi@gmail.com>
2024-12-07 00:19:44 +0700

    feat: navigation

--- app/src/main/java/com/bangkit/classifund/ui/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/bangkit/classifund/ui/navigation/BottomNavigationBar.kt
@@ -1,9 +1,18 @@
 package com.bangkit.classifund.ui.navigation
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.PieChart
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 @Composable
@@ -12,19 +21,45 @@ fun BottomNavigationBar(navController: NavHostController) {
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("home") },
-            icon = { Text("🏠") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Home",
+                    tint = Color(27, 191, 168, )
+                ) },
             label = { Text("Home") }
         )
         NavigationBarItem(
             selected = false,
             onClick = { navController.navigate("add_transaction") },
-            icon = { Text("+") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add",
+                    tint = Color(27, 191, 168, )
+                ) },
             label = { Text("Add") }
         )
         NavigationBarItem(
             selected = false,
+            onClick = { navController.navigate("analytics") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.PieChart,
+                    contentDescription = "Analytics",
+                    tint = Color(27, 191, 168, )
+                ) },
+            label = { Text("Analytics") }
+        )
+        NavigationBarItem(
+            selected = false,
             onClick = { navController.navigate("settings") },
-            icon = { Text("⚙️") },
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "Settings",
+                    tint = Color(27, 191, 168, )
+                ) },
             label = { Text("Settings") }
         )
     }
