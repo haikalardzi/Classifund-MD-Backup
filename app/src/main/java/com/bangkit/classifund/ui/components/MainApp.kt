a34d2eac7326a82300ece770c220a14796e98d96
henryanandsr <henryanandseptianradityo@gmail.com>
2024-12-10 20:14:08 +0700

    fix : remove navbar after logout

--- app/src/main/java/com/bangkit/classifund/ui/components/MainApp.kt
+++ app/src/main/java/com/bangkit/classifund/ui/components/MainApp.kt
@@ -3,7 +3,9 @@ package com.bangkit.classifund.ui.components
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.bangkit.classifund.ui.navigation.BottomNavigationBar
 import com.bangkit.classifund.ui.navigation.NavigationGraph
@@ -11,10 +13,19 @@ import com.bangkit.classifund.ui.navigation.NavigationGraph
 @Composable
 fun MainApp() {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
 
     Scaffold(
-        bottomBar = { BottomNavigationBar(navController = navController) }
+        bottomBar = {
+            if (currentRoute != "login") {
+                BottomNavigationBar(navController = navController)
+            }
+        }
     ) { innerPadding ->
-        NavigationGraph(navController = navController, modifier = Modifier.padding(innerPadding))
+        NavigationGraph(
+            navController = navController,
+            modifier = Modifier.padding(innerPadding)
+        )
     }
 }
